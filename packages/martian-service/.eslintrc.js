4b2fa69db3ebf29cad82ccd36d6edb92d6cdd7db
Andrey <smirnov.andrey.n@gmail.com>
2021-03-18 14:02:42 +0300

    chore: add typescript, nodemon

--- packages/martian-service/.eslintrc.js
+++ packages/martian-service/.eslintrc.js
@@ -4,11 +4,12 @@ module.exports = {
     commonjs: true,
     es2021: true,
   },
-  extends: ['eslint:recommended', 'prettier'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
   parserOptions: {
     ecmaVersion: 12,
   },
-  plugins: ['prettier'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['prettier', '@typescript-eslint'],
   rules: {
     'prettier/prettier': ['error', { endOfLine: 'auto' }],
     'no-use-before-define': 'off',
---
+++ packages/martian-service/src/app.ts
@@ -0,0 +1,5 @@
+import { b } from './test';
+const app = (a: number) => a + b;
+console.log(app(9));
+console.log(15);
+console.log('DO');
